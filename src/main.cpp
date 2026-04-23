c8a1afe0264996ef90a98e2e688eeb8042cb997e
Ryan Hausmann <ryanhausmann3@gmail.com>
2026-04-22 20:26:09 -0400

    Add utility to calculate band pass FIR taps for given sampling and low/high cutoff frequencies

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,19 @@ int main()
     // source: sine wave
     SinusoidalSource<double> source (
         1000000,
-        2 * M_PI * 300000,
+        2 * M_PI * 450000,
         0
     );
 
     // FIR filter
-    vector<double> firTaps = FIRCoefficientCalculator<double>::calculateHighPassCoefficients(1000000, 200000, 18);
+    vector<double> firTaps = FIRCoefficientCalculator<double>::calculateBandPassCoefficients(1000000, 100000, 400000, 18);
     FIRFilter<double> firFilter(source, firTaps);
 
     // console output
     std::cout << std::fixed << std::setprecision(3);
     ConsoleSink<double> sink(firFilter);
 
-    sink.printSamples(0, 25);
+    sink.printSamples(0, 100);
 
     return 0;
 }
--- src/utils/FIRCoefficientCalculator.hpp
+++ src/utils/FIRCoefficientCalculator.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 using std::vector;
+using std::min;
 
 /**
  * Represents a rectangular window (all values within window are 1)
@@ -83,6 +84,26 @@ public:
 
         return taps;
     }
+
+    /**
+     * Computes coefficients/taps for a high-pass FIR filter.
+     *
+     * @param samplingFreq The sampling frequency of the signal.
+     * @param cutoffFreqLow The lower cutoff frequency of the filter.
+     * @param cutoffFreqHigh The higher cutoff frequency of the filter.
+     * @param M The order of the filter (must be even).
+     * @param windowFunction (optional) The window function. Default: rectangular window
+     */
+    static vector<T> calculateBandPassCoefficients(T samplingFreq, T cutoffFreqLow, T cutoffFreqHigh, int M, T(*windowFunction)(int, int) = rectangularWindow<T>)
+    {
+        // Create a low pass and a high pass filter with the given frequencies
+        vector<T> lowpassTaps = calculateLowPassCoefficients(samplingFreq, cutoffFreqHigh, M, windowFunction);
+        vector<T> highpassTaps = calculateHighPassCoefficients(samplingFreq, cutoffFreqLow, M, windowFunction);
+
+        // Convolute the taps to create a band pass taps
+        vector<T> taps = convolute(lowpassTaps, highpassTaps);
+        return taps;
+    }
     
 private:
     /**
@@ -93,5 +114,24 @@ private:
         return (x == (T)0)
             ? (T)1
             : sin(M_PI * x) / (M_PI * x);
+    }
+
+    /**
+     * Calculates the convolution of two signals
+     */
+    static vector<T> convolute(vector<T> a, vector<T> b)
+    {
+        size_t convolutionN = a.size() + b.size() - 1;
+        vector<T> out(convolutionN, (T)0);
+
+        for (size_t i = 0; i < a.size(); i++)
+        {
+            for (size_t j = 0; j < b.size(); j++)
+            {
+                out[i + j] += a[i] * b[j];
+            }
+        }
+
+        return out;
     }
 };
