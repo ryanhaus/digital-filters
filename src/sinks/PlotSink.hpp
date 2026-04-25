126e0acce902d9d1156f74e4733f54749e9efe34
Ryan Hausmann <ryanhausmann3@gmail.com>
2026-04-25 18:59:09 -0400

    Reconfigure PlotSink to plot real and imaginary components of the signal separately

--- src/sinks/PlotSink.hpp
+++ src/sinks/PlotSink.hpp
@@ -16,34 +16,42 @@ public:
      */
     PlotSink(Signal<T>& signal)
         : SignalSink<T>(signal)
-    {}
+    {
+        figure_handle f = figure();
+        f->backend()->run_command("unset warnings"); // avoid annoying gnuplot warnings
+    }
 
     /**
      * Plots samples over a given range to the console.
      *
      * @param nSamples Number of samples to take
-     * @param startBound The starting bound of the plot (defaults to '')
-     * @param endBound The starting bound of the plot (defaults to 'end')
+     * @param startBound The starting bound of the plot
+     * @param endBound The starting bound of the plot
      */
     figure_handle plotSamples(size_t nSamples, T startBound, T endBound)
     {
-        figure_handle f = figure();
-        f->backend()->run_command("unset warnings"); // avoid annoying gnuplot warnings
-                                                           
         vector<double> freq = linspace(startBound, endBound, nSamples);
-        vector<double> response;
-        response.reserve(nSamples);
+        vector<double> responseReal;
+        vector<double> responseImaginary;
+
+        responseReal.reserve(nSamples);
+        responseImaginary.reserve(nSamples);
 
         for (size_t n = 0; n < nSamples; n++)
         {
             complex<T> sample = this->signal.nextSample();
-            response.push_back(abs(sample));
+            responseReal.push_back(sample.real());
+            responseImaginary.push_back(sample.imag());
         }
 
-        plot(freq, response)->line_width(2);
+        hold(on);
+        plot(freq, responseReal)->line_width(2);
+        plot(freq, responseImaginary)->line_width(2);
         show();
 
         return f;
     }
+
 private:
+    figure_handle f;
 };
