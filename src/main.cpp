12c656e2793b21d900cf2285733159773c844cd1
Ryan Hausmann <ryanhausmann3@gmail.com>
2026-05-02 23:34:36 -0400

    Use polar discriminator to find change in phase instead of modulus method

--- src/main.cpp
+++ src/main.cpp
@@ -51,29 +51,25 @@ int main()
     // decimate to 240 kHz to reduce processing
     Decimator<float> decimated240kHz(firFilterLP, 10);
 
-    // convert from complex samples to arg (arctan), note that the signal should now be real-only
-    ComplexToArg<float> arg(decimated240kHz);
-
-    // take derivative of phase to demodulate FM
-    SampleDifferentiator<float> differentiator(arg);
-
-    // restrain within boundaries of [-pi, pi]
-    CustomFilter<float> modulus(
-        differentiator,
+    // compute polar discriminator to find the change in phase
+    CustomFilter<float> phaseChange(
+        decimated240kHz,
         [](complex<float> sample)
         {
-            return fmod(sample.real() + M_PI, 2 * M_PI) - M_PI;
+            static complex<float> prevSamp = 0;
+
+            complex<float> discr = sample * std::conj(prevSamp);
+            prevSamp = sample;
+
+            return std::arg(discr);
         }
     );
-
-    // scale appropriately to match expected amplitude
-    MultiplierFilter<float> mult(modulus, 1 / M_PI);//2 * fmDeviation / (samplingFreq/10));
 
     // FM deemphasis (approximate)
     vector<float> firTapsDeemph =
         FIRCoefficientCalculator<float>::calculateLowPassCoefficients(240e3, 15000, 300, blackmanWindow);
 
-    FIRFilter<float> firFilterDeemph(mult, firTapsDeemph);
+    FIRFilter<float> firFilterDeemph(phaseChange, firTapsDeemph);
 
     // decimate again to 48 kHz
     Decimator<float> decimated48kHz(firFilterDeemph, 5);
